c0b8604ca4caf3b939f1edb62717922eabde03f0
Bl4ky113 <bl4ky@proton.me>
2025-02-28 23:39:10 -0500

    Removed christofides from release

--- delta_tsp/algorithms/__init__.py
+++ delta_tsp/algorithms/__init__.py
@@ -5,15 +5,15 @@ from .neighbor_delta import neighbor_delta_algorithm
 from .neighbor_2otp import neighbor_opt_algorithm
 
 ALGORITHMS_LIST: list[dict[str, any]] = [
-    {
-        "uid": 0,
-        "name": "Christofides",
-        "info_file": "christofides.json",
-        "function": christofides_algorithm,
-        "icon": "",
-        "fg": "",
-        "bg": ""
-    },
+    # {
+        # "uid": 0,
+        # "name": "Christofides",
+        # "info_file": "christofides.json",
+        # "function": christofides_algorithm,
+        # "icon": "",
+        # "fg": "",
+        # "bg": ""
+    # },
     {
         "uid": 1,
         "name": "2-OPT",
